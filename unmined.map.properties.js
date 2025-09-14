b4a2dcbfe588069a5ab104a9c1b0b402d9c18cd4
Miningmanias <Ryan@metzke.us>
2025-09-13 23:58:52 -0500

    Automated map update - Sat 09/13/2025 23:58:52.32

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -3,7 +3,7 @@ var UnminedMapProperties = {
     minZoom: -6,
     maxZoom: 0,
     defaultZoom: 0,
-    imageFormat: "jpeg",
+    imageFormat: "webp",
     minRegionX: -11,
     minRegionZ: -18,
     maxRegionX: 1953,
